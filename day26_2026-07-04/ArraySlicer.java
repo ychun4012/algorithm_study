8334b001bba4d5819ee2e76b0847ee7d4fa8d57e
ychun4012 <ychun4012@gmail.com>
2026-07-05 00:02:02 +0900

    day26 add

---
+++ day26_2026-07-04/ArraySlicer.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        
+        switch (n) {
+            case 1 -> {
+                for (int i = 0; i <= slicer[1]; i++) {
+                    list.add(num_list[i]);
+                }
+            }
+            case 2 -> {
+                for (int i = slicer[0]; i < num_list.length; i++) {
+                    list.add(num_list[i]);
+                }
+            }
+            case 3 -> {
+                for (int i = slicer[0]; i <= slicer[1]; i++) {
+                    list.add(num_list[i]);
+                }
+            }
+            case 4 -> {
+                for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+                    list.add(num_list[i]);
+                }
+            }
+        }
+        
+        int [] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+        
+    }
+}
